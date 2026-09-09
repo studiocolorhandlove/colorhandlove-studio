6974b6596fedc5f61d280af1f35967f5d397e9c2
studiocolorhandlove <studio.colorhandlove@gmail.com>
2026-09-09 12:44:19 +0200

    Add files via upload
    Prima versione ColorHandLove Studio

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='chl-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','index.html','manifest.json','assets/logo-circle.png','assets/logo-horizontal.png','assets/colorino.png']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
